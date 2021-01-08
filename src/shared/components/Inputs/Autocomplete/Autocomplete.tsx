02d5db49725b9639d6e6123e143bb94e347aba43
M-Kolacz <michal.kolacz45@gmail.com>
2021-01-08 21:47:42 +0100

    Create Autocomplete input.

---
+++ src/shared/components/Inputs/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Field } from 'formik';
+import {
+    Autocomplete as MUIAutocomplete,
+    AutocompleteRenderInputParams,
+} from 'formik-material-ui-lab';
+import { Grid, TextField } from '@material-ui/core';
+
+import { GridProps } from '../../../interfaces/MaterialUI';
+import Label, { LabelProps } from '../Label/Label';
+
+export interface AutocompleteProps extends LabelProps, GridProps {
+    name: string;
+    options: { title: string }[];
+}
+
+export const Autocomplete: React.FC<AutocompleteProps> = ({
+    xs,
+    sm,
+    md,
+    lg,
+    xl,
+    id,
+    labelTitle,
+    name,
+    options,
+}) => {
+    return (
+        <>
+            <Label labelTitle={labelTitle} id={id} />
+            <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl}>
+                <Field
+                    name={name}
+                    component={MUIAutocomplete}
+                    options={options}
+                    getOptionLabel={(option: any) => option.title}
+                    style={{ width: '100%' }}
+                    renderInput={(params: AutocompleteRenderInputParams) => (
+                        <TextField {...params} label='Wybierz' variant='outlined' />
+                    )}
+                />
+            </Grid>
+        </>
+    );
+};
--- src/shared/components/Inputs/index.tsx
+++ src/shared/components/Inputs/index.tsx
@@ -1,3 +1,4 @@
 export { DatePicker } from './DatePicker/DatePicker';
 export { TimePicker } from './TimePicker/TimePicker';
 export { RadioGroup } from './RadioGroup/RadioGroup';
+export { Autocomplete } from './Autocomplete/Autocomplete';
