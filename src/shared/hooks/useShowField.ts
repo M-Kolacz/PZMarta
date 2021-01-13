bd4e7e102d843923fb7a294a400891fa5188603e
M-Kolacz <michal.kolacz45@gmail.com>
2021-01-13 20:49:35 +0100

    Change logic useShowField
    Delete unecessary high order function.

--- src/shared/hooks/useShowField.ts
+++ src/shared/hooks/useShowField.ts
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 import { Event } from '../types/event';
 
-export type useShowFieldProps = [boolean, () => (event: Event) => void];
+export type useShowFieldProps = [boolean, (event: Event) => void];
 
 export const useShowField = (condition: string) => {
     const [showPersonDeath, setShowPersonDeath] = useState(false);
 
-    const showField = () => {
-        return (event: Event) => {
-            if (event.target.value === condition) {
-                setShowPersonDeath(true);
-            } else {
-                setShowPersonDeath(false);
-            }
-        };
+    const showField = (event: Event) => {
+        if (event.target.value === condition) {
+            setShowPersonDeath(true);
+        } else {
+            setShowPersonDeath(false);
+        }
     };
     return [showPersonDeath, showField] as useShowFieldProps;
 };
