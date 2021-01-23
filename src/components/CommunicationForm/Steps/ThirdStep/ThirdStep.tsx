3b62e51d69509055ce4d6a041fe161525aae099f
M-Kolacz <michal.kolacz45@gmail.com>
2021-01-23 18:26:05 +0100

    ThirdStep folder structure

---
+++ src/components/CommunicationForm/Steps/ThirdStep/ThirdStep.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface ThirdStepProps {}
+
+const ThirdStep: React.FC<ThirdStepProps> = () => {
+    return <h1>Third step</h1>;
+};
+
+export default ThirdStep;
---
+++ src/components/CommunicationForm/Steps/ThirdStep/data.ts
@@ -0,0 +1,33 @@
+export const tst = 'tst';
+
+const DAMAGE_OWNER = 'damageOwner';
+const DAMAGE_OWNER_LEASING = 'damageOwnerLeasing';
+const APPLICANT_NAME = 'applicantName';
+const APPLICANT_SURNAME = 'applicantSurname';
+const APPLICANT_PHONE = 'applicantPhone';
+const APPLICANT_EMAIL = 'applicantEmail';
+const APPLICANT_ADDRESS_CODE = 'applicantAddressCode';
+const APPLICANT_CITY = 'applicantCity';
+const APPLICANT_STREET = 'applicantStreet';
+const APPLICANT_LOCAL_NUMBER = 'applicantLocalNumber';
+const APPLICANT_PESEL_ = 'applicantPesel';
+
+const OWNER_NAME = 'ownerName';
+const OWNER_SURNAME = 'ownerSurname';
+const OWNER_PHONE = 'ownerPhone';
+const OWNER_EMAIL = 'ownerEmail';
+const OWNER_ADDRESS_CODE = 'ownerAddressCode';
+const OWNER_CITY = 'ownerCity';
+const OWNER_STREET = 'ownerStreet';
+const OWNER_LOCAL_NUMBER = 'ownerLocalNumber';
+const OWNER_PESEL_ = 'ownerPesel';
+
+const CONTACT_NAME = 'contactName';
+const CONTACT_SURNAME = 'contactSurname';
+const CONTACT_PHONE = 'contactPhone';
+const CONTACT_EMAIL = 'contactEmail';
+const CONTACT_ADDRESS_CODE = 'contactAddressCode';
+const CONTACT_CITY = 'contactCity';
+const CONTACT_STREET = 'contactStreet';
+const CONTACT_LOCAL_NUMBER = 'contactrLocalNumber';
+const CONTACT_PESEL_ = 'contactPesel';
