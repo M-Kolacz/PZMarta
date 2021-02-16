0e6425f4158766b6183de7f39ef6d48c920aa606
M-Kolacz <michal.kolacz45@gmail.com>
2021-02-16 17:57:41 +0100

    Add .trim() to exsisting validators

--- src/shared/SSOT/schemas/yup.ts
+++ src/shared/SSOT/schemas/yup.ts
@@ -4,20 +4,24 @@ import { requiredField, emailField, passwordField } from './errorDescriptions';
 
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
 
-export const dateValidation = Yup.string().required(requiredField).nullable();
+export const dateValidation = Yup.string().trim().required(requiredField).nullable();
 
-export const stringValidation = Yup.string().required(requiredField);
+export const stringValidation = Yup.string().trim().required(requiredField);
 
 export const objectValidation = Yup.object().required(requiredField).nullable();
 
-export const emailValidation = Yup.string().required(requiredField).email(emailField);
+export const emailValidation = Yup.string().trim().required(requiredField).email(emailField);
 
-export const passwordValidation = Yup.string().required(requiredField).matches(passwordRegex, {
-    message: passwordField,
-});
+export const passwordValidation = Yup.string()
+    .trim()
+    .required(requiredField)
+    .matches(passwordRegex, {
+        message: passwordField,
+    });
 
 export const confirmField = (field: string, message: string) =>
     Yup.string()
+        .trim()
         .required(requiredField)
         .oneOf([Yup.ref(field), null], message);
 
