c19d39b8d3916c742fa61c8ff784a9dc308290e3
M-Kolacz <michal.kolacz45@gmail.com>
2021-02-19 13:48:18 +0100

    Improve styling of LoginForm

--- src/components/LoginPage/LoginForm/LoginFormStyles.ts
+++ src/components/LoginPage/LoginForm/LoginFormStyles.ts
@@ -2,14 +2,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     Form: {
-        width: '94%',
-        margin: '10px auto',
+        width: '100%',
+        padding: theme.spacing(1.25, 2),
     },
     ErrorContainer: {
         textAlign: 'left',
     },
     Error: {
-        color: 'red',
+        color: theme.palette.error.main,
     },
     FormElementContainer: {
         textAlign: 'right',
